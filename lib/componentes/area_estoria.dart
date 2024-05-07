3fb66da044fbeaa8bf998a9553b50ed66aa871b6
TarsisGuerra <tarsisguerra@hotmail.com>
2024-05-07 00:37:33 -0300

    detalhesStories

--- lib/componentes/area_estoria.dart
+++ lib/componentes/area_estoria.dart
@@ -1,6 +1,8 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:fb_flutter/componentes/imagem_perfil.dart';
 import 'package:fb_flutter/dados/dados.dart';
 import 'package:fb_flutter/modelos/modelos.dart';
+import 'package:fb_flutter/uteis/paleta_cores.dart';
 import 'package:flutter/material.dart';
 
 class AreaStoria extends StatelessWidget {
@@ -21,14 +23,24 @@ class AreaStoria extends StatelessWidget {
       child: ListView.builder(
         padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 10),
         scrollDirection: Axis.horizontal,
-        itemCount: estorias.length,
+        itemCount: 1 + estorias.length,
         itemBuilder: (contect, indice) {
-          Estoria estoria = estorias[indice];
+          if (indice == 0) {
+            Estoria estoriaUsuario = Estoria(
+                usuario: usuarioAtual, urlImagem: usuarioAtual.urlImagem);
+
+            return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 4),
+                child: CartaoEstoria(estoria: estoriaUsuario));
+          }
+
+          Estoria estoria = estorias[indice - 1];
 
           return Padding(
             padding: const EdgeInsets.symmetric(horizontal: 4),
             child: CartaoEstoria(
               estoria: estoria,
+              adicionarEstoria: false,
             ),
           );
         },
@@ -39,9 +51,11 @@ class AreaStoria extends StatelessWidget {
 
 class CartaoEstoria extends StatelessWidget {
   Estoria estoria;
+  bool adicionarEstoria;
 
   CartaoEstoria({
     required this.estoria,
+    this.adicionarEstoria = true,
     super.key,
   });
 
@@ -58,7 +72,51 @@ class CartaoEstoria extends StatelessWidget {
               height: double.infinity,
               width: 110,
               fit: BoxFit.cover,
-            ))
+            )),
+        Container(
+          height: double.infinity,
+          width: 110,
+          decoration: BoxDecoration(
+              gradient: PaletaCores.degradeEstoria,
+              borderRadius: BorderRadius.circular(12)),
+        ),
+        Positioned(
+            top: 8,
+            left: 8,
+            child: adicionarEstoria
+                ? Container(
+                    height: 40,
+                    width: 40,
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      shape: BoxShape.circle,
+                    ),
+                    child: IconButton(
+                      padding: EdgeInsets.zero,
+                      icon: const Icon(Icons.add),
+                      iconSize: 30,
+                      color: PaletaCores.azulFacebook,
+                      onPressed: () {},
+                    ),
+                  )
+                : ImagemPerfil(
+                    urlImagem: estoria.usuario.urlImagem,
+                    foiVisualizado: estoria.foiVisualizado,
+                  )),
+        Positioned(
+          bottom: 8,
+          left: 8,
+          right: 8,
+          child: Text(
+            adicionarEstoria ? "Criar Story" : estoria.usuario.nome,
+            style: const TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+          ),
+        )
       ],
     );
   }
--- lib/uteis/paleta_cores.dart
+++ lib/uteis/paleta_cores.dart
@@ -3,4 +3,9 @@ import 'package:flutter/material.dart';
 class PaletaCores {
 //static é usado para acessar sem precisar isntanciar
   static const Color azulFacebook = Color(0xFF1777F2);
+  static const LinearGradient degradeEstoria = LinearGradient(
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+    colors: [Colors.transparent, Colors.black54],
+  );
 }
